3370a029a94d92df85ad505641425d2be10eeaac
umesh-awasthi <umesh.awasthi@mangoitsolutions.in>
2025-05-26 12:30:46 +0530

    adminentry branch merge

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ Route::get('/', [AdminAuthController::class, 'showLoginForm'])->name('login');
 Route::post('/admin/login', [AdminAuthController::class, 'login']);
 Route::match(['get', 'post'], '/admin/layout/settings', [SettingController::class, 'layoutForm'])->name('admin.category.layout')->middleware(['auth', 'admin']);
 Route::post('/admin/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
+
